43858ab547722679f5ddb29e4312646346cafb7b
san123452 <rlarkdtks14@gmail.com>
2026-03-11 14:21:02 +0900

    오류 잡아서 배포

--- src/main/java/com/ess/erp/EssErpApplication.java
+++ src/main/java/com/ess/erp/EssErpApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EssErpApplication {
 
 	public static void main(String[] args) {
+		// [추가] XML 보안 정책을 완화해서 MyBatis DTO 로딩 에러를 해결합니다.
+		System.setProperty("javax.xml.accessExternalDTD", "all"); 
+		
 		SpringApplication.run(EssErpApplication.class, args);
 	}
 
